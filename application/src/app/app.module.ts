1fcb764c5da6b6bf14db74a8269eb337b9ce6dc1
flash <k.kostov.eu@gmail.com>
2017-08-29 16:45:45 +0300

    base skeleton

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PetitionModule } from './petition/petition.module';
+import { UserModule } from './user/user.module';
+import { HomeModule } from './home/home.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PetitionModule,
+    UserModule,
+    HomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
