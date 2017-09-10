636e6472b913a5e1f82f01fe8ed2f2a6616e1bf4
flash <k.kostov.eu@gmail.com>
2017-09-10 18:34:28 +0300

    successful login component isolated unit test mock and component test mock with injected stub dependencies

--- application/src/app/user/login/login.component.spec.ts
+++ application/src/app/user/login/login.component.spec.ts
@@ -1,3 +1,6 @@
+import { ToastsManager, ToastOptions } from 'ng2-toastr/ng2-toastr';
+import { FirebaseAuthService } from './../firebase-auth.service';
+import { FormsModule } from '@angular/forms';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -8,10 +11,31 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let authService: any;
 
   beforeEach(async(() => {
+
+    let firebaseServiceStub = {
+      login: jasmine.createSpy('login').and.returnValue(true)
+    };
+
+    let toastsManagerServiceStub = {
+      error: jasmine.createSpy('error').and.returnValue(true)
+    };
+
+    let toastsOptionsServiceStub = {
+      showCloseButton: true
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [FormsModule],
+      providers: [
+        {provide: FirebaseAuthService, useValue: firebaseServiceStub},
+        {provide: ToastsManager, useValue: toastsManagerServiceStub},
+        {provide: ToastOptions, useValue: toastsOptionsServiceStub}
+      ]
     })
     .compileComponents();
   }));
@@ -19,10 +43,40 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
+
+    authService = TestBed.get(FirebaseAuthService);
+    de = fixture.debugElement;
+
   });
 
   it('should create', () => {
+    console.log(de);
     expect(component).toBeTruthy();
+  });
+});
+
+describe('LoginComponent Isolated Unit Tests', () => {
+  let firebaseServiceStub: any;
+  let toastsManagerServiceStub: any;
+  let toastsOptionsServiceStub: any;
+
+  beforeEach(() => {
+    firebaseServiceStub = {
+      login: jasmine.createSpy('login').and.returnValue(true)
+    };
+
+    toastsManagerServiceStub = {
+      error: jasmine.createSpy('error').and.returnValue(true)
+    };
+
+    toastsOptionsServiceStub = {
+      showCloseButton: true
+    };
+  });
+
+  it('init test', () => {
+    const sut = new LoginComponent(firebaseServiceStub, toastsManagerServiceStub, toastsOptionsServiceStub);
+    expect(sut).toBeTruthy();
   });
 });
