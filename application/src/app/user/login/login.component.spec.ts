72ddafa16f3292c39bc8378c388f26b4c03b03fd
flash <k.kostov.eu@gmail.com>
2017-09-10 19:28:56 +0300

    login component isolated unit tests completed

--- application/src/app/user/login/login.component.spec.ts
+++ application/src/app/user/login/login.component.spec.ts
@@ -60,6 +60,7 @@ describe('LoginComponent Isolated Unit Tests', () => {
   let firebaseServiceStub: any;
   let toastsManagerServiceStub: any;
   let toastsOptionsServiceStub: any;
+  let sut: LoginComponent;
 
   beforeEach(() => {
     firebaseServiceStub = {
@@ -71,12 +72,75 @@ describe('LoginComponent Isolated Unit Tests', () => {
     };
 
     toastsOptionsServiceStub = {
-      showCloseButton: true
+      showCloseButton: false
     };
+
+    sut = new LoginComponent(firebaseServiceStub, toastsManagerServiceStub, toastsOptionsServiceStub);
   });
 
-  it('init test', () => {
-    const sut = new LoginComponent(firebaseServiceStub, toastsManagerServiceStub, toastsOptionsServiceStub);
+  it('LoginComponent should exist', () => {
     expect(sut).toBeTruthy();
+  });
+
+  it('LoginComponent should initiate with undefined email and password', () => {
+    expect(sut.email).toBeUndefined();
+    expect(sut.password).toBeUndefined();
+  });
+
+  it('LoginComponent should call authService login if password and email are valid', () => {
+    sut.password = '1234567';
+    sut.email = 'kalin@test.com';
+    sut.login();
+    expect(firebaseServiceStub.login.calls.count()).toBe(1);
+  });
+
+  it('LoginComponent should NOT call authService login if password is not valid', () => {
+    sut.password = '1';
+    sut.email = 'kalin@test.com';
+    sut.login();
+    expect(firebaseServiceStub.login.calls.count()).toBe(0);
+  });
+
+  it('LoginComponent should NOT call authService login if email is not valid', () => {
+    sut.password = '1234567';
+    sut.email = 'invalid';
+    sut.login();
+    expect(firebaseServiceStub.login.calls.count()).toBe(0);
+  });
+
+  it('LoginComponent should call toastr error if password is not valid', () => {
+    sut.password = '1';
+    sut.email = 'kalin@test.com';
+    sut.login();
+    expect(toastsManagerServiceStub.error.calls.count()).toBe(1);
+  });
+
+  it('LoginComponent should call toastr error if email is not valid', () => {
+    sut.password = '1234567';
+    sut.email = 'invalid';
+    sut.login();
+    expect(toastsManagerServiceStub.error.calls.count()).toBe(1);
+  });
+
+  it('LoginComponent should call toastr error with correct message if email is not valid', () => {
+    sut.password = '1234567';
+    sut.email = 'invalid';
+    sut.login();
+    expect(toastsManagerServiceStub.error).toHaveBeenCalledWith('Invalid Email');
+  });
+
+  it('LoginComponent should call toastr error with correct message if password is not valid', () => {
+    sut.password = '1';
+    sut.email = 'kalin@test.com';
+    sut.login();
+    expect(toastsManagerServiceStub.error).toHaveBeenCalledWith('Password must be atleast 6 chars long.');
+  });
+
+  it('LoginComponent should reset the email and password fields after successful login call', () => {
+    sut.password = '123456';
+    sut.email = 'kalin@test.com';
+    sut.login();
+    expect(sut.email).toEqual('');
+    expect(sut.password).toEqual('');
   });
 });
