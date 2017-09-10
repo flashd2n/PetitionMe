ad7ad4c54e450923469a643ff04dbc7993833c3a
flash <k.kostov.eu@gmail.com>
2017-09-11 00:44:33 +0300

    cleaned unused specs, app stable

--- application/src/app/petition/directives/high-light-petition.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighLightPetitionDirective } from './high-light-petition.directive';
-
-describe('HighLightPetitionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighLightPetitionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- application/src/app/petition/directives/petition-progressbar.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PetitionTooltipDirective } from './petition-tooltip.directive';
-
-describe('PetitionTooltipDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PetitionTooltipDirective();
-    expect(directive).toBeTruthy();
-  });
-});
