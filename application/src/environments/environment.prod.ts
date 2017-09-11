0c7f972f0acd05dc4689e7567e4dcdd1c1eeb89b
flash <k.kostov.eu@gmail.com>
2017-09-11 15:09:43 +0300

    deploy config sync

--- application/src/environments/environment.prod.ts
+++ application/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAP04SM_2-57rSvPGSMGgX942bAjhbbdas',
+    authDomain: 'the-deranged-eagles.firebaseapp.com',
+    databaseURL: 'https://the-deranged-eagles.firebaseio.com',
+    projectId: 'the-deranged-eagles',
+    storageBucket: 'the-deranged-eagles.appspot.com',
+    messagingSenderId: '450306476766'
+  }
 };
